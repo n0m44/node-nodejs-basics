2dcb881c72f05fc6f0aacb50e771747d7ea180b2
gleb <gmararash@naumen.ru>
2024-10-07 00:48:13 +0500

    feat: impl src/hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import * as fs from 'fs';
+import * as path from 'path'
+import * as crypto from 'crypto'
+import * as stream from 'stream/promises'
+
 const calculateHash = async () => {
-    // Write your code here 
+    const targetFilePath = path.join(import.meta.dirname, 'files', 'fileToCalculateHashFor.txt');
+
+    const readStream = fs.createReadStream(targetFilePath);
+
+    stream.pipeline(readStream, crypto.createHash('sha256').setEncoding('hex'), async (hashInstance) => {
+        hashInstance
+          .toArray()
+          .then((hashList) => {
+            console.log(hashList[0]);
+        });
+    });
 };
 
 await calculateHash();
